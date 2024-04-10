91177ec74e980154fe60f4472d01d7ca757e712a
Vincent Kiefer <viki00002@stud.uni-saarland.de>
2024-04-10 13:30:38 +0200

    see last commit

---
+++ code/initial_values.py
@@ -0,0 +1,10 @@
+from universal_constants import *
+from material_parameters import *
+
+def show_initial_values():
+	pass
+
+if __name__ == "__main__":
+	import pandas as pd
+	show_initial_values()
+
---
+++ code/universal_constants.py
@@ -0,0 +1,11 @@
+from better_symbol import Sym
+
+"""
+Creates `better_symbol.Sym()` instances for universal constants in seperate namespace
+"""
+
+m_e = Sym("m_e", "Ruhemasse eines Elektrons")
+q_e = Sym("e", "Elementarladung")
+eps_0 = Sym("epsilon_0", "Dielektrizitätskonstante des Vakuums")
+h = Sym("h", "Planksches Wirkungsquantum")
+k = Sym("k", "Boltzmann-Konstante")

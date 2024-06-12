c456d8e040fad1f831220f108e9aabd962243112
HerrGrafVal <vincent-kiefer@web.de>
2024-06-12 19:19:47 +0200

    Improved ODE solution visualisation (see Figure 3) Note that rho(x) should be zero outside the SCR. This should be addressed in the future

--- scripts/DDM_ode.py
+++ scripts/DDM_ode.py
@@ -1,9 +1,9 @@
 import numpy as np
 import mpmath as mp
 from functools import cache
-from cache import pickle_read
+from cache import pickle_read, pickle_save
 from read_dataframe import fill_values
-from symbols import h, q_e, k, m_e, eps, W_g, N_v, N_c, m_ec, m_hc, T, U_D, N_d, N_a, x_p0, x_n0
+from symbols import h, q_e, k, m_e, eps, W_g, N_v, N_c, m_ec, m_hc, T, U_D, N_d, N_a, x_p0, x_n0, WIDTH
 
 # ----------------------------------------------------------------------------
 
@@ -41,164 +41,217 @@ def fun_n(x, z):
 
 # ----------------------------------------------------------------------------
 
-"""
-Prepare variables of type float
-"""
+if __name__ == "__main__":
 
-e = float(fill_values(q_e))  # In J
-k = float(fill_values(k)) / e  # In eV/K
-T = float(fill_values(T))  # In K
-kT = k * T  # In eV
-U_D = float(fill_values(U_D))  # In V
-eps = float(fill_values(eps))  # In C/Vm
+    """
+    Prepare variables of type float
+    """
 
-W_g = float(fill_values(W_g, eV=True))  # In eV
+    e = float(fill_values(q_e))  # In J
+    k = float(fill_values(k)) / e  # In eV/K
+    T = float(fill_values(T))  # In K
+    kT = k * T  # In eV
+    U_D = float(fill_values(U_D))  # In V
+    eps = float(fill_values(eps))  # In C/Vm
 
-# In m^-3
-N_v = float(fill_values(N_v))
-N_c = float(fill_values(N_c))
-N_dp = float(fill_values(N_d))  # Assume ionisation of all dotation atoms
-N_am = float(fill_values(N_a))  # Assume ionisation of all dotation atoms
+    W_g = float(fill_values(W_g, eV=True))  # In eV
 
-x_p0 = float(fill_values(x_p0))
-x_n0 = float(fill_values(x_n0))
+    # In m^-3
+    N_v = float(fill_values(N_v))
+    N_c = float(fill_values(N_c))
+    N_dp = float(fill_values(N_d))  # Assume ionisation of all dotation atoms
+    N_am = float(fill_values(N_a))  # Assume ionisation of all dotation atoms
 
-# ----------------------------------------------------------------------------
+    x_p0 = float(fill_values(x_p0))
+    x_n0 = float(fill_values(x_n0))
 
-"""
-Define band structure
-"""
+    WIDTH = float(fill_values(WIDTH))
 
-W_v = 0
-W_c = W_g
-try:
-    W_F = pickle_read("Fermi_level")  # In eV
-except FileNotFoundError:
-    print("Fermi_level.pkl not found in SAVE_FOLDER. Execute fermi_level.py and try again.")
+    # ----------------------------------------------------------------------------
 
-# ----------------------------------------------------------------------------
+    """
+    Define band structure
+    """
 
-"""
-SciPy sole_ivp (method = RK45) yielded unsatisfactory results, other methods fail to work see previous commit
-mpmath odefun (method = taylor) produces satisfactory results, but can't iterate backwards
+    W_v = 0
+    W_c = W_g
+    try:
+        W_F = pickle_read("Fermi_level")  # In eV
+    except FileNotFoundError:
+        print("Fermi_level.pkl not found in SAVE_FOLDER. Execute fermi_level.py and try again.")
 
-x_p, x_n are defined by fieldstrength reaching zero and continuity in x = 0
-We assume they can be found in [2*x_p0 ; 2*x_n0]
+    # ----------------------------------------------------------------------------
 
-     z0 = phi
-z  = z1 = E
-     
-rho = DDM(z0)[4]
+    """
+    SciPy sole_ivp (method = RK45) yielded unsatisfactory results, other methods fail to work see previous commit
+    mpmath odefun (method = taylor) produces satisfactory results, but can't iterate backwards
 
-dz   z0. = -z1
-dx = z1. = rho/eps
-"""
+    x_p, x_n are defined by fieldstrength reaching zero and continuity in x = 0
+    We assume they can be found in [2*x_p0 ; 2*x_n0]
 
-STEPS = 100
+         z0 = phi
+    z  = z1 = E
+         
+    rho = DDM(z0)[4]
 
-# p-region
-phi_p = []
-E_p = []
-W_v_p = []
-W_c_p = []
-p_p = []
-n_p = []
-rho_p = []
-# n-region
-phi_n = []
-E_n = []
-W_v_n = []
-W_c_n = []
-p_n = []
-n_n = []
-rho_n = []
+    dz   z0. = -z1
+    dx = z1. = rho/eps
+    """
 
-# ----------------------------------------------------------------------------
+    STEPS = 100
 
-"""
-Generate phi, E values for STEPS amount of x values per area
-"""
+    # p-region
+    phi_p = []
+    E_p = []
+    W_v_p = []
+    W_c_p = []
+    p_p = []
+    n_p = []
+    rho_p = []
+    # n-region
+    phi_n = []
+    E_n = []
+    W_v_n = []
+    W_c_n = []
+    p_n = []
+    n_n = []
+    rho_n = []
 
-stepsize_p = -x_p0*2/STEPS
-stepsize_n = x_n0*2/STEPS
+    # ----------------------------------------------------------------------------
 
-x_span_p = np.arange(0, -x_p0 * 2, stepsize_p).astype(mp.mpf)
-x_span_n = np.arange(0, x_n0 * 2, stepsize_n).astype(mp.mpf)
+    """
+    Generate phi, E values for STEPS amount of x values per area
+    """
 
-sol_p = mp.odefun(fun_p, 0, [0, 0])
-sol_n = mp.odefun(fun_n, 0, [0, 0])
+    stepsize_p = -x_p0*2/STEPS
+    stepsize_n = x_n0*2/STEPS
 
-for x in x_span_p:
-    z = sol_p(x)
-    phi = -z[0]
-    E = -z[1]
-    phi_p.append(phi)
-    E_p.append(E)
+    x_span_p = np.arange(0, -x_p0 * 2, stepsize_p).astype(mp.mpf)
+    x_span_n = np.arange(0, x_n0 * 2, stepsize_n).astype(mp.mpf)
 
-zz_p = np.array([phi_p, E_p])
+    sol_p = mp.odefun(fun_p, 0, [0, 0])
+    sol_n = mp.odefun(fun_n, 0, [0, 0])
 
-for x in x_span_n:
-    z = sol_n(x)
-    phi = U_D + z[0]
-    E = -z[1]
-    phi_n.append(phi)
-    E_n.append(E)
+    for x in x_span_p:
+        z = sol_p(x)
+        phi = -z[0]
+        E = -z[1]
+        phi_p.append(phi)
+        E_p.append(E)
 
-zz_n = np.array([phi_n, E_n])
+    zz_p = np.array([phi_p, E_p])
 
-# ----------------------------------------------------------------------------
+    for x in x_span_n:
+        z = sol_n(x)
+        phi = U_D + z[0]
+        E = -z[1]
+        phi_n.append(phi)
+        E_n.append(E)
 
-"""
-Check where zz_p best meets zz_n in terms of phi AND E
-"""
+    zz_n = np.array([phi_n, E_n])
 
-DELTA = []
-for k, zz_P in enumerate(zz_p.T[1:,:]):
-    for j, zz_N in enumerate(zz_n.T[1:,:]):
-        delta = abs([1, 1] - np.divide(zz_P, zz_N))
-        DELTA.append([k + 1, j + 1, delta])
-best_total = min(DELTA, key = lambda x: x[2][0] + x[2][1])
+    # ----------------------------------------------------------------------------
 
-index_p = best_total[0]
-index_n = best_total[1]
+    """
+    Check where zz_p best meets zz_n in terms of phi AND E
+    """
 
-# p-area
-# crop all lists to end at approximated continuity
-# xx_p needs to be reversed
-xx_p = -1 * x_span_p[:index_p + 1]
-xx_p = np.flip(xx_p)
-xx_p -= stepsize_p # Only for improved visualisation
-phi_p = phi_p[:index_p + 1]
-E_p = E_p[:index_p + 1]
+    DELTA = []
+    for k, zz_P in enumerate(zz_p.T[1:,:]):
+        for j, zz_N in enumerate(zz_n.T[1:,:]):
+            delta = abs([1, 1] - np.divide(zz_P, zz_N))
+            DELTA.append([k + 1, j + 1, delta])
+    best_total = min(DELTA, key = lambda x: x[2][0] + x[2][1])
 
-# n-area
-# crop all lists to end at approximated continuity
-# phi and E need to be reversed
-xx_n = x_span_n[:index_n + 1]
-xx_n += stepsize_n # Only for improved visualisation
-phi_n = phi_n[:index_n + 1]
-phi_n.reverse()
-E_n = E_n[:index_n + 1]
-E_n.reverse()
+    index_p = best_total[0]
+    index_n = best_total[1]
 
-# ----------------------------------------------------------------------------
+    # p-area
+    # crop all lists to end at approximated continuity
+    # xx_p needs to be reversed
+    xx_p = -1 * x_span_p[:index_p + 1]
+    xx_p = np.flip(xx_p)
+    #xx_p -= stepsize_p # Only for improved visualisation
+    phi_p = phi_p[:index_p + 1]
+    E_p = E_p[:index_p + 1]
 
-"""
-Evaluate DDM for all relevant potentials
-"""
+    # n-area
+    # crop all lists to end at approximated continuity
+    # phi and E need to be reversed
+    xx_n = x_span_n[:index_n + 1]
+    xx_n += stepsize_n # Prevent overlap at x = 0
+    phi_n = phi_n[:index_n + 1]
+    phi_n.reverse()
+    E_n = E_n[:index_n + 1]
+    E_n.reverse()
 
-for phi in phi_p:
-    ddm = DDM_p(phi)
-    W_v_p.append(ddm[0])
-    W_c_p.append(ddm[1])
-    p_p.append(mp.log10(ddm[2]))
-    n_p.append(mp.log10(ddm[3]))
-    rho_p.append(ddm[4])
+    # ----------------------------------------------------------------------------
 
-for phi in phi_n:
-    ddm = DDM_n(phi)
-    W_v_n.append(ddm[0])
-    W_c_n.append(ddm[1])
-    p_n.append(mp.log10(ddm[2]))
-    n_n.append(mp.log10(ddm[3]))
-    rho_n.append(ddm[4])
+    """
+    Evaluate DDM for all relevant potentials
+    """
+
+    for phi in phi_p:
+        ddm = DDM_p(phi)
+        W_v_p.append(ddm[0])
+        W_c_p.append(ddm[1])
+        p_p.append(mp.log10(ddm[2]))
+        n_p.append(mp.log10(ddm[3]))
+        rho_p.append(ddm[4])
+
+    for phi in phi_n:
+        ddm = DDM_n(phi)
+        W_v_n.append(ddm[0])
+        W_c_n.append(ddm[1])
+        p_n.append(mp.log10(ddm[2]))
+        n_n.append(mp.log10(ddm[3]))
+        rho_n.append(ddm[4])
+
+    # ----------------------------------------------------------------------------
+
+    """
+    Join p and n values together
+    """
+
+    xx = list(xx_p) + list(xx_n)
+    phi = phi_p + phi_n
+    E = E_p + E_n
+    W_v = W_v_p + W_v_n
+    W_c = W_c_p + W_c_n
+    p = p_p + p_n
+    n = n_p + n_n
+
+    # Charge density jumps at x=0 -> better visualisation with two values at x = 0
+    xx_rho = list(xx_p) + list(xx_n - stepsize_n)
+    rho = rho_p + rho_n
+
+    # ----------------------------------------------------------------------------
+
+    """
+    Include neutral areas
+    """
+
+    x_p = xx[0]
+    x_n = xx[-1]
+    w_p = WIDTH * x_p
+    w_n = WIDTH * x_n
+
+    xx = [w_p] + xx + [w_n]
+    xx_rho = [w_p] + xx_rho + [w_n]
+
+    results = {
+        "W_F" : W_F,
+        "x_p" : x_p,
+        "x_n" : x_n,
+        "w_p" : w_p,
+        "w_n" : w_n,
+        "xx" : xx,
+        "xx_rho" : xx_rho
+        }
+
+    for name in ["phi", "E", "W_v", "W_c", "p", "n", "rho"]:
+        values = eval(name)
+        results[name] = [values[0]] + values + [values[-1]]
+
+    pickle_save(results, "ODE_solution")
+    print("Results saved successfully!")
--- scripts/generate_ode_graphs.py
+++ scripts/generate_ode_graphs.py
@@ -1,8 +1,23 @@
-from DDM_ode import xx_p, phi_p, E_p, W_v_p, W_c_p, p_p, n_p, rho_p
-from DDM_ode import xx_n, phi_n, E_n, W_v_n, W_c_n, p_n, n_n, rho_n
-from DDM_ode import W_F
+from cache import pickle_read
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter
+
+results = pickle_read("ODE_solution")
+xx = results["xx"]
+xx_rho = results["xx_rho"]
+phi = results["phi"]
+E = results["E"]
+W_v = results["W_v"]
+W_c = results["W_c"]
+W_F = results["W_F"]
+p = results["p"]
+n = results["n"]
+rho = results["rho"]
+
+"""
+ToDo:
+Add x_p, x_n tick labels
+"""
 
 form_x = EngFormatter(unit = "$m$")
 form_cc = EngFormatter(unit = "$m^{-3}$")
@@ -15,42 +30,34 @@ fig, (plt_cc, plt_rho, plt_E, plt_phi, plt_W) = plt.subplots(5,1)
 fig.subplots_adjust(hspace = 1.5)
 
 plt_cc.set_title("Ladungsträgerdichten")
-plt_cc.plot(xx_p, p_p, "C0", label = "$log_{10}$ $p (x)$")
-plt_cc.plot(xx_n, p_n, "C0")
-plt_cc.plot(xx_p, n_p, "C1", label = "$log_{10}$ $n (x)$")
-plt_cc.plot(xx_n, n_n, "C1")
+plt_cc.plot(xx, p, "C0", label = "$log_{10}$ $p (x)$")
+plt_cc.plot(xx, n, "C1", label = "$log_{10}$ $n (x)$")
 plt_cc.xaxis.set_major_formatter(form_x)
 plt_cc.yaxis.set_major_formatter(form_cc)
 plt_cc.legend()
 
 plt_rho.set_title("Raumladungsdichte")
-plt_rho.plot(xx_p, rho_p, "C0", label = "$\\rho (x)$")
-plt_rho.plot(xx_n, rho_n, "C0")
+plt_rho.plot(xx_rho, rho, "C0", label = "$\\rho (x)$")
 plt_rho.xaxis.set_major_formatter(form_x)
 plt_rho.yaxis.set_major_formatter(form_rho)
 plt_rho.legend()
 
 plt_E.set_title("Feldstärke")
-plt_E.plot(xx_p, E_p, "C0", label = "$E (x)$")
-plt_E.plot(xx_n, E_n, "C0")
+plt_E.plot(xx, E, "C0", label = "$E (x)$")
 plt_E.xaxis.set_major_formatter(form_x)
 plt_E.yaxis.set_major_formatter(form_E)
 plt_E.legend()
 
 plt_phi.set_title("Potential")
-plt_phi.plot(xx_p, phi_p, "C0", label = "$\\varphi (x)$")
-plt_phi.plot(xx_n, phi_n, "C0")
+plt_phi.plot(xx, phi, "C0", label = "$\\varphi (x)$")
 plt_phi.xaxis.set_major_formatter(form_x)
 plt_phi.yaxis.set_major_formatter(form_phi)
 plt_phi.legend()
 
 plt_W.set_title("Bandverläufe")
-plt_W.plot(xx_p, W_v_p, "C0", label = "$W_V (x)$")
-plt_W.plot(xx_n, W_v_n, "C0")
-plt_W.plot(xx_p, W_c_p, "C1", label = "$W_C (x)$")
-plt_W.plot(xx_n, W_c_n, "C1")
-plt_W.plot([xx_p[0], xx_n[-1]], [W_F, W_F], "C2", label = "$W_F$")
-# plt_W.plot([xx_p[0], xx_p[-1]], [W_F, W_F], "C2", label = "$W_F$")
+plt_W.plot(xx, W_v, "C0", label = "$W_V (x)$")
+plt_W.plot(xx, W_c, "C1", label = "$W_C (x)$")
+plt_W.plot([xx[0], xx[-1]], [W_F, W_F], "C2", label = "$W_F$")
 plt_W.xaxis.set_major_formatter(form_x)
 plt_W.yaxis.set_major_formatter(form_W)
 plt_W.legend()
